af98488391a5508de1124f54d2b51b69ace8e920
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-03-25 13:55:48 +0300

    Add features, fix bugs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Bounces or redirects requests to missing static files. 
-Partially inspired by http://menendez.com/blog/using-django-as-pass-through-image-proxy/
+Bounces or redirects requests to missing static files.
+Partially inspired by [http://menendez.com/blog/using-django-as-pass-through-image-proxy/](http://menendez.com/blog/using-django-as-pass-through-image-proxy/)
 
 I.e. could be useful when you want to run the server with production database locally
 and have user uploaded content fetched transparently from production site.
@@ -8,7 +8,8 @@ Options:
     :mode - [ :off,
               :bounce, # returns 404 to any request to static URL,
               :fallback ] # any request to static URL is redirected to options[:fallback_static_url]
-    :static_path_regex - Regexp which matches the path to your static files. Along the lines of the Capistrano conventions defaults to `%r{/system/(audios|photos|videos)}`
+    :static_path_regex - Regexp which matches the path to your static files.
+                         Along the lines of the Capistrano conventions defaults to `%r{/system/(audios|photos|videos)}`
     :fallback_static_url - URL of the production site
 
 To use with Rails add the following to your config/environments/development.rb:
--- lib/rack/static_fallback.rb
+++ lib/rack/static_fallback.rb
@@ -1,6 +1,6 @@
 module Rack
   # Bounces or redirects requests to missing static files.
-  # Partially inspired by http://menendez.com/blog/using-django-as-pass-through-image-proxy/
+  # Partially inspired by [http://menendez.com/blog/using-django-as-pass-through-image-proxy/](http://menendez.com/blog/using-django-as-pass-through-image-proxy/)
   #
   # I.e. could be useful when you want to run the server with production database locally
   # and have user uploaded content fetched transparently from production site.
@@ -9,7 +9,8 @@ module Rack
   #     :mode - [ :off,
   #               :bounce, # returns 404 to any request to static URL,
   #               :fallback ] # any request to static URL is redirected to options[:fallback_static_url]
-  #     :static_path_regex - Regexp which matches the path to your static files. Along the lines of the Capistrano conventions defaults to `%r{/system/(audios|photos|videos)}`
+  #     :static_path_regex - Regexp which matches the path to your static files.
+  #                          Along the lines of the Capistrano conventions defaults to `%r{/system/(audios|photos|videos)}`
   #     :fallback_static_url - URL of the production site
   #
   # To use with Rails add the following to your config/environments/development.rb:
